51fc9ee331ad59e7ec6993d79557389869f25abf
Dean Petty <dean.petty@gmail.com>
2024-01-07 22:50:59 +0000

    Had to downgrade to React 17 to keep this boat floating with MUI issues.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
+const root = document.getElementById('root');
+render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>, root);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
